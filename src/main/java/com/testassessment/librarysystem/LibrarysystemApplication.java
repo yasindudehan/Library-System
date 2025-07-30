ba1cb7e731b8e5420649d6892d1fc7c25aa74730
dehan <ruwanwellayasindu@gmail.com>
2025-07-30 15:10:04 +0530

    initial commit

---
+++ src/main/java/com/testassessment/librarysystem/LibrarysystemApplication.java
@@ -0,0 +1,13 @@
+package com.testassessment.librarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarysystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testassessment/librarysystem/LibrarysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testassessment.librarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
